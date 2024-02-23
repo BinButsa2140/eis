7e748843f39fab7b0fed5db2102d1e50e03a07e4
binbutsa2140 <bam.356345@gmail.com>
2024-02-23 16:18:28 +0700

    scrolling hero

--- src/contents/hero.tsx
+++ src/contents/hero.tsx
@@ -1,34 +1,39 @@
 export const herodata = [
     {
-        type:'',
-        imgpath:'https://images.unsplash.com/photo-1542291026-7eec264c27ff?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        type:'shoe',
+        data:[
+            'https://images.unsplash.com/photo-1542291026-7eec264c27ff?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+            'https://images.unsplash.com/photo-1600185365483-26d7a4cc7519?q=80&w=1925&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+            'https://images.unsplash.com/photo-1549298916-b41d501d3772?q=80&w=1712&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+            'https://images.unsplash.com/photo-1516478177764-9fe5bd7e9717?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+            'https://images.unsplash.com/photo-1494496195158-c3becb4f2475?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+            'https://images.unsplash.com/flagged/photo-1556637640-2c80d3201be8?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+            
+
+        ]
     },
     {
-        type:'',
-        imgpath:'https://images.unsplash.com/photo-1612724271076-24c0a3bade29?q=80&w=1925&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        type:'Handbag',
+        data:[
+            'https://images.unsplash.com/photo-1591348278863-a8fb3887e2aa?q=80&w=1717&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+            'https://images.unsplash.com/photo-1587467512961-120760940315?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+            'https://images.unsplash.com/photo-1548036328-c9fa89d128fa?q=80&w=1769&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+            'https://images.unsplash.com/photo-1605733513597-a8f8341084e6?q=80&w=1858&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+            'https://images.unsplash.com/photo-1588056421330-045073ab19b7?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+
+
+        ]
     },
     {
-        type:'',
-        imgpath:'https://images.unsplash.com/photo-1549298916-b41d501d3772?q=80&w=1712&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        type:'Fragrance',
+        data:[
+            'https://images.unsplash.com/photo-1591892153927-33f7a2ffec89?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+            'https://images.unsplash.com/photo-1592842125302-53494d164c90?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+            'https://images.unsplash.com/photo-1523293182086-7651a899d37f?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+            'https://images.unsplash.com/photo-1587017539504-67cfbddac569?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+            'https://images.unsplash.com/photo-1547887537-6158d64c35b3?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+            ''
+            
+        ]
     },
-    {
-        type:'',
-        imgpath:"https://images.unsplash.com/photo-1597633125097-5a9961e1f03d?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-    },
-    {
-        type:'',
-        imgpath:'https://images.unsplash.com/photo-1587467512961-120760940315?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-    },
-    {
-        type:'',
-        imgpath:'https://images.unsplash.com/photo-1600680725775-21c5927dbf74?q=80&w=1876&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-    },
-    {
-        type:'',
-        imgpath:'https://images.unsplash.com/photo-1610113145494-5c7c9e6b0cfe?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-    },
-    {
-        type:'',
-        imgpath:'https://images.unsplash.com/photo-1592914567737-30212032ca25?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-    }
 ]
--- src/sections/Hero/index.tsx
+++ src/sections/Hero/index.tsx
@@ -1,22 +1,25 @@
 import { herodata } from "../../contents/hero"
 export default function Hero() {
+    
   return (
-    <div className='grid grid-cols-[40%_60%] h-screen mt-20'>
+    <div className='grid grid-cols-[40%_60%] h-screen mt-20 m-10'>
         <div className=" h-96 flex flex-col justify-center w-full lg:mt-20">
             <div className="text-left px-5">
                 <h1 className="text-5xl font-bold uppercase mb-5">Real or fake?</h1>
                 <p className="text-xl mb-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, corrupti.</p>
-                <button className="bg-blue-500 px-4 py-2 rounded-lg btn-primary">Start now!</button>
+                <button className="bg-yellow-400 px-4 py-2 rounded-lg btn-primary">Start now!</button>
             </div>
             
         </div>
-        <div className="w-full grid gap-10 ps-4 carousel h-96 ">
-                {herodata.map((data, index) => (
-                    <div key={index} className="bg-primary shadow-blue-800 shadow-lg 
-                    carousel-item  w-full rounded-2xl ">
-                        <img className="w-full h-98 object-cover brightness-75" src={data.imgpath} alt="" />
-                    </div>
-                ))}
+        <div className="w-full grid grid-rows-3 gap-5 mt-0 h-96">
+            {herodata.map((data, index) => (
+                <div key={index} className="carousel border relative flex flex-col border-yellow-400">
+                    <h1 className="bottom-0 right-0 absolute">{data.type}</h1>
+                    {data.data.map((pix, index2) =>(
+                        <img className="w-full h-32 object-cover brightness-125" key={index2} src={pix} alt="" />
+                    ))}
+                </div>
+            ))}
         </div>
     </div>
   )
