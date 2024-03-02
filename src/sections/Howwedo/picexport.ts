e0f376ed6a9e82de1f8e9a7921ef375b8118a136
Jaoshiba <blackspell909@gmail.com>
2024-03-03 00:20:16 +0700

    /

---
+++ src/sections/Howwedo/picexport.ts
@@ -0,0 +1,8 @@
+// yourcategory, takeapic, certi, hand2hand
+
+import yourcategory from '../../assets/yourcategory.png'
+import takeapic from '../../assets/'
+
+export {
+    yourcategory
+}
--- src/sections/Howwedo/picexport.tsx
+++
@@ -1,9 +0,0 @@
-// takeapic, certi, hand2hand
-
-import yourcategory from '../../assets/yourcategory.png'
-
-
-
-export => {
-    
-}
